43f508b4ba23ecdf27d64bd9226df657fd2931b3
adrian <adrian@adrian-PC>
2020-04-17 09:41:39 +0300

    initial commit

---
+++ gwb-web/src/main/java/com/adrian/gwbweb/GwbWebApplication.java
@@ -0,0 +1,13 @@
+package com.adrian.gwbweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GwbWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GwbWebApplication.class, args);
+	}
+
+}
---
+++ gwb-web/src/test/java/com/adrian/gwbweb/GwbWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrian.gwbweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GwbWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
